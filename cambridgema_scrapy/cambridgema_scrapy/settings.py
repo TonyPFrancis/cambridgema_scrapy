2857255452d6286caea57b6721eaf70a844132f6
Jithin <cyb3rjithu@gmail.com>
2015-06-10 21:43:20 +0530

    starting spider cambridgema_scrapy

---
+++ cambridgema_scrapy/cambridgema_scrapy/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for cambridgema_scrapy project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'cambridgema_scrapy'
+
+SPIDER_MODULES = ['cambridgema_scrapy.spiders']
+NEWSPIDER_MODULE = 'cambridgema_scrapy.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'cambridgema_scrapy (+http://www.yourdomain.com)'
