478c254ccc88e66863eb9532c8e80a3d68982ef9
TonyPFrancis <tony.612.4u@gmail.com>
2015-06-13 14:20:04 +0530

    updating spider for pagination

--- cambridgema_scrapy/cambridgema_scrapy/spiders/cambridgema_spider.py
+++ cambridgema_scrapy/cambridgema_scrapy/spiders/cambridgema_spider.py
@@ -2,6 +2,7 @@ from scrapy import Spider, Selector
 from scrapy.http import FormRequest
 import re
 from scrapy.shell import inspect_response
+from time import sleep
 
 
 class CambridgemaSpider(Spider):
@@ -9,6 +10,19 @@ class CambridgemaSpider(Spider):
     allowed_domains = ['cambridgema.gov']
     start_urls = ['https://www.cambridgema.gov/propertydatabase/',]
     BASE_URL = 'https://www.cambridgema.gov'
+    PAGINATION_FIRST_INDEX = 1
+    PAGINATION_LAST_INDEX = 1272
+    HEADERS = {'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+               'Accept-Language':'en-US,en;q=0.8',
+               'Cache-Control':'max-age=0',
+               'Connection':'keep-alive',
+               'Host':'www.cambridgema.gov',
+               'Origin':'https://www.cambridgema.gov',
+               'Referer':'https://www.cambridgema.gov/propertydatabase/',
+               'User-Agent':'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36'}
+    COOKIES = {'_ga':'GA1.2.1291057140.1433952157',
+               '_gat':'1',
+               'style':'default'}
 
     def parse(self, response):
         sel = Selector(response)
@@ -47,35 +61,7 @@ class CambridgemaSpider(Spider):
                   'ctl00$Primary$PropertyDBSearch$txtAdvLotNum':'',
                   'ctl00$Primary$PropertyDBSearch$txtAdvUnit':'',
                   'ctl00$Primary$PropertyDBSearch$btnAdvancedSearchSubmit':'Search'}
-        yield FormRequest(url=url, formdata=params, callback=self.get_database_list_limit)
-
-    def get_database_list_limit(self, response):
-        sel = Selector(response)
-
-        LAST_INDEX_XPATH = '//table[@id="gvSearchResults"]/tbody/tr/td[@colspan="7"]/table/tr/td/a[text()="Last Page"]/@href'
-        __VIEWSTATE_XPATH = '//input[@id="__VIEWSTATE"]/@value'
-        __VIEWSTATEGENERATOR_XPATH = '//input[@id="__VIEWSTATEGENERATOR"]/@value'
-        __EVENTVALIDATION_XPATH = '//input[@id="__EVENTVALIDATION"]/@value'
-
-        last_page_link = sel.xpath(LAST_INDEX_XPATH).extract()
-        last_page_link = last_page_link[0].strip() if last_page_link else ''
-        if last_page_link:
-            ids = re.findall(r'\(\'(.*)?\',\'(.*)?\'', last_page_link, re.I)
-            target, argument = ids[0] if ids else ('', '')
-            if target and argument:
-                __VIEWSTATE = sel.xpath(__VIEWSTATE_XPATH).extract()
-                __VIEWSTATE = __VIEWSTATE[0] if __VIEWSTATE else ''
-                __VIEWSTATEGENERATOR = sel.xpath(__VIEWSTATEGENERATOR_XPATH).extract()
-                __VIEWSTATEGENERATOR = __VIEWSTATEGENERATOR[0] if __VIEWSTATEGENERATOR else ''
-                __EVENTVALIDATION = sel.xpath(__EVENTVALIDATION_XPATH).extract()
-                __EVENTVALIDATION = __EVENTVALIDATION[0] if __EVENTVALIDATION else ''
-                url = 'https://www.cambridgema.gov/propertydatabase/'
-                params = {'__EVENTTARGET': target,
-                          '__EVENTARGUMENT': argument,
-                          '__VIEWSTATE': __VIEWSTATE,
-                          '__VIEWSTATEGENERATOR': __VIEWSTATEGENERATOR,
-                          '__EVENTVALIDATION': __EVENTVALIDATION}
-                yield FormRequest(url=url, formdata=params, callback=self.parse_database_list)
+        yield FormRequest(url=url, formdata=params, callback=self.parse_database_list, dont_filter=True, headers=self.HEADERS, cookies=self.COOKIES)
 
     def parse_database_list(self, response):
         sel = Selector(response)
@@ -83,8 +69,6 @@ class CambridgemaSpider(Spider):
         PROPERTY_DB_XPATH = '//table[@id="gvSearchResults"]/tbody/tr/td[2]/a/@href'
 
         property_db_links = sel.xpath(PROPERTY_DB_XPATH).extract()
-        print "*** property_db_links"
-        print property_db_links
         if property_db_links:
             for property_db_link in property_db_links:
                 property_db_url = self.BASE_URL+property_db_link
@@ -93,20 +77,13 @@ class CambridgemaSpider(Spider):
         else:
             return
 
-        NEXT_PAGE_XPATH = '//table[@id="gvSearchResults"]/tbody/tr/td[@colspan="7"]/table/tr/td[span]/following-sibling::td[1]/a/@href'
-        __VIEWSTATE_XPATH = '//input[@id="__VIEWSTATE"]/@value'
-        __VIEWSTATEGENERATOR_XPATH = '//input[@id="__VIEWSTATEGENERATOR"]/@value'
-        __EVENTVALIDATION_XPATH = '//input[@id="__EVENTVALIDATION"]/@value'
+        if self.PAGINATION_FIRST_INDEX <= self.PAGINATION_LAST_INDEX:
+            __VIEWSTATE_XPATH = '//input[@id="__VIEWSTATE"]/@value'
+            __VIEWSTATEGENERATOR_XPATH = '//input[@id="__VIEWSTATEGENERATOR"]/@value'
+            __EVENTVALIDATION_XPATH = '//input[@id="__EVENTVALIDATION"]/@value'
 
-        next_page_link = sel.xpath(NEXT_PAGE_XPATH).extract()
-        next_page_link = next_page_link[0].strip() if next_page_link else ''
-        print "*** next_page_link"
-        print next_page_link
-        if not next_page_link:
-            inspect_response(response)
-        if next_page_link:
-            ids = re.findall(r'\(\'(.*)?\',\'(.*)?\'', next_page_link, re.I)
-            target, argument = ids[0] if ids else ('', '')
+            self.PAGINATION_FIRST_INDEX = self.PAGINATION_FIRST_INDEX + 1
+            target, argument = 'ctl00$Primary$PropertyDBSearch$gvSearchResults', 'Page$'+str(self.PAGINATION_FIRST_INDEX)
             print "*** target, argument"
             print target, argument
             if target and argument:
@@ -122,5 +99,7 @@ class CambridgemaSpider(Spider):
                           '__VIEWSTATE': __VIEWSTATE,
                           '__VIEWSTATEGENERATOR': __VIEWSTATEGENERATOR,
                           '__EVENTVALIDATION': __EVENTVALIDATION}
-                yield FormRequest(url=url, formdata=params, callback=self.parse_database_list)
+                print params
+                sleep(10)
+                yield FormRequest(url=url, formdata=params, callback=self.parse_database_list, dont_filter=True, headers=self.HEADERS, cookies=self.COOKIES)
 
